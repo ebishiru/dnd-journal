72f71f87f61a7d9c349d43be6a07764d8c027184
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2025-12-06 00:55:23 -0500

    Removed trailing / in BE

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const deleteCampaign = require("./handlers/deleteCampaign");
 const app = express();
 
 app.use(cors({
-    origin: `https://dungeonnotesdatabase.vercel.app/`,
+    origin: `https://dungeonnotesdatabase.vercel.app`,
     credentials: true
 }))
 
