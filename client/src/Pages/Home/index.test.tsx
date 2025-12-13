d7f7ac9af1711f806fa34d21a3c8e62cf1a70e84
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2025-12-12 22:40:10 -0500

    Added jest test cases for Home Page

--- client/src/Pages/Home/index.test.tsx
+++ client/src/Pages/Home/index.test.tsx
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/react";
+import { getByTestId, render } from "@testing-library/react";
 import Home from "../Home/index";
 import { CurrentUserContext } from "../../Context/CurrentUserContext";
 import { BrowserRouter } from "react-router-dom";
 
-test("Home Page Paragraph text 1 appears", () => {
+test("Home Page Paragraph texts appears", () => {
     const { getByText } = render(
         <BrowserRouter>
             <CurrentUserContext.Provider value={[null, jest.fn()]}>
@@ -13,4 +13,33 @@ test("Home Page Paragraph text 1 appears", () => {
     );
 
     expect(getByText(/Every campaign starts small/i)).toBeInTheDocument();
+    expect(getByText(/Dungeon Notes Database/i)).toBeInTheDocument();
+    expect(getByText(/Take a seat and write your tale/i)).toBeInTheDocument();
 }) 
+
+test("Log in button appears when not logged in", () => {
+    const { getByText } = render(
+        <BrowserRouter>
+            <CurrentUserContext.Provider value={[null, jest.fn()]}>
+                <Home />
+            </CurrentUserContext.Provider>
+        </BrowserRouter>
+    )
+
+    expect(getByText("Log In")).toBeInTheDocument();
+})
+
+test("Manage info and Log out button appears when logged in", () => {
+    const mockSetUser = jest.fn();
+    const { getByText, queryByText } = render(
+        <BrowserRouter>
+            <CurrentUserContext.Provider value={["Test.Account", mockSetUser]}>
+                <Home />
+            </CurrentUserContext.Provider>
+        </BrowserRouter>
+    )
+
+    expect(getByText("Log Out")).toBeInTheDocument();
+    expect(getByText("Manage Info")).toBeInTheDocument();
+    expect(queryByText("Log In")).toBeNull();
+})
