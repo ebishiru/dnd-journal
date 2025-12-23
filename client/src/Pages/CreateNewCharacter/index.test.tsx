ec9d283e73d8ab60d83d6ea265ba2e75ad2ba2a4
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2025-12-23 12:52:09 -0500

    Added tests to createcharacter

--- client/src/Pages/CreateNewCharacter/index.test.tsx
+++ client/src/Pages/CreateNewCharacter/index.test.tsx
@@ -41,3 +41,34 @@ test("Form renders when logged in", () => {
     expect(screen.getByLabelText(/story/i)).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /create character/i})).toBeInTheDocument();
 })
+
+test("Quotes can be added and removed.", async () => {
+    renderComponent("Kevin");
+    await screen.findByText(/Add more quotes/i);
+    const addQuoteButton = screen.getByRole("button", {name: /add more quotes/i});
+    const removeQuoteButton = screen.getByRole("button", {name: /remove last quote/i});
+    await userEvent.click(addQuoteButton);
+    expect(await screen.getByLabelText(/2/)).toBeInTheDocument();
+    await userEvent.click(removeQuoteButton);
+    expect(screen.queryByLabelText(/2/)).not.toBeInTheDocument();
+})
+
+test("Character can be created", async () => {
+    (global.fetch as jest.Mock).mockResolvedValue({
+        json: async () => ({
+            status: 201,
+            message: "Character successfully created.",
+        })
+    })
+    renderComponent("Kevin");
+    await screen.findByText(/summon forth/i);
+    await userEvent.type(screen.getByLabelText(/name/i),"Test Name");
+    await userEvent.type(screen.getByLabelText(/story/i),"Test Story");
+    await userEvent.type(screen.getByLabelText(/1/),"Test Quote");
+    const characterCreateButton = screen.getByRole("button", {name: /create character/i});
+    await userEvent.click(characterCreateButton);
+
+    expect(global.fetch).toHaveBeenCalled();
+    expect(toast.success).toHaveBeenCalledWith("Character successfully created.");
+    expect(toast.error).not.toHaveBeenCalled();
+})
